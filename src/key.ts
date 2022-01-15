f994dd355a5d48cc020c437deec9295c3394b7b8
liushooter <shooter.hero@gmail.com>
2022-01-15 10:52:44 +0800

    gear keyring example

---
+++ src/key.ts
@@ -0,0 +1,18 @@
+import fs from 'fs'
+import { GearKeyring } from '@gear-js/api'
+
+const jsonKeyring = fs.readFileSync('./account/key.json').toString()
+
+const keyring = GearKeyring.fromJson(jsonKeyring, "11111111")
+
+const _pub = keyring.publicKey
+const _addressRaw = keyring.addressRaw
+
+let publicKey = "0x" + Buffer.from(_pub).toString('hex')
+let addressRaw = "0x" + Buffer.from(_addressRaw).toString('hex')
+
+console.log("address type:", keyring.type)
+console.log("publicKey: ", publicKey)
+console.log("address Raw:", addressRaw)
+console.log("address:", keyring.address)
+console.log("meta:", keyring.meta)
