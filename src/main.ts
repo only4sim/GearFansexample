e681494b82ada0b8e77830e1fb594ca1e322e6ca
liushooter <shooter.hero@gmail.com>
2022-01-15 10:46:00 +0800

    connect gear node example

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { GearApi } from '@gear-js/api'
+
+async function connect() {
+  const rpc = "wss://rpc-node.gear-tech.io"
+  const workshopRpc = "wss://node-workshop.gear.rs/"
+
+  const gearApi = await GearApi.create({
+    providerAddress: rpc
+  })
+
+  const [chain, nodeName, nodeVersion] = await Promise.all([
+    gearApi.chain(),
+    gearApi.nodeName(),
+    gearApi.nodeVersion(),
+  ])
+
+  console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`)
+
+  gearApi.gearEvents.subscribeNewBlocks((header) => {
+    console.log(`New block with number: ${header.number.toNumber()} and hash: ${header.hash.toHex()}`)
+  })
+
+  return gearApi
+}
+
+connect().catch(console.error)
