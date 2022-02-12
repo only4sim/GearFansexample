901a79ea470909fa6d71eacb659610a46685c530
liushooter <shooter.hero@gmail.com>
2022-02-12 16:08:54 +0800

    receive event data

---
+++ src/event.ts
@@ -0,0 +1,66 @@
+const fs = require("fs")
+import {
+  GearApi,
+  LogData,
+  getWasmMetadata,
+  CreateType,
+  MessageDispatchedData,
+} from '@gear-js/api'
+
+async function getMessageDispatchedEvent() {
+  const rpc = "wss://rpc-node.gear-tech.io"
+  // const workshopRpc = "wss://node-workshop.gear.rs/"
+
+  const gearApi = await GearApi.create({
+    providerAddress: rpc
+  })
+
+  gearApi.allEvents((events) => {
+    events.forEach(async ({
+      event: { data, method }
+    }) => {
+      console.log({ method })
+
+      if (method == "MessageDispatched") {
+        const msg = new MessageDispatchedData(data)
+
+        const messageId = msg.messageId.toHex()
+        const outcome = msg.outcome
+
+        console.log({ messageId })
+        console.log({ outcome })
+
+        console.log("isSuccess", outcome.isSuccess)
+        console.log("isFailure", outcome.isFailure)
+
+        if (outcome.isFailure) {
+          const errMsg = outcome.asFailure.toHuman()
+          console.log({ errMsg })
+        } else {
+          const okMsg = outcome.asSuccess.toHuman()
+          console.log({ okMsg })
+        }
+
+      }
+
+      if (method == "Log") {
+        const logdata = new LogData(data)
+        const payload = logdata.payload
+
+        const metaFileBuffer = fs.readFileSync('./wasm/fungible_token.meta.wasm')
+
+        const meta = await getWasmMetadata(metaFileBuffer)
+        let type = meta.handle_output!
+
+        let decoded = CreateType.decode(type, payload, meta)
+        console.log(/---payload decode---/)
+        console.log(JSON.stringify(decoded))
+        console.log(/---payload decode---/)
+      }
+
+    })
+
+  })
+}
+
+getMessageDispatchedEvent()
