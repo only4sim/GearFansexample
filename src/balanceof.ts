bcccca0999d7a75bb158b688c8934cd1004a9b87
liushooter <shooter.hero@gmail.com>
2022-04-24 14:55:15 +0800

    up

--- src/balanceof.ts
+++ src/balanceof.ts
@@ -1,54 +1,29 @@
 import fs from 'fs'
-import { GearApi, GearKeyring, getWasmMetadata } from '@gear-js/api'
-import { exit } from 'process'
+import { GearApi, GearKeyring } from '@gear-js/api'
 
 const jsonKeyring = fs.readFileSync('./account/key.json').toString()
 const keyring = GearKeyring.fromJson(jsonKeyring, "11111111")
-const metaFileBuffer = fs.readFileSync('./wasm/fungible_token.meta.wasm')
 
-async function sendMsg() {
-  const rpc = "wss://rpc-node.gear-tech.io"
-  // const workshopRpc = "wss://node-workshop.gear.rs"
+const metapath = "./wasm/fungible_token.meta.wasm"
+const metaFileBuffer = fs.readFileSync(metapath)
+const programId = "0x1d0867c638d109b6d5b7336660e7688cacb939f1d1508062dd6d648389a9eaa6"
+
+async function balance() {
+  const rpc = "wss://rpc-node.gear-tech.io:443"
 
   const gearApi = await GearApi.create({
     providerAddress: rpc
   })
 
-  const payload = {
-    "BalanceOf": "5CyREBErNFhogptd92dtC8ybuoUczVYh2ijvdhTpS2PJGeq7"
-  }
+  const addressRaw = keyring.addressRaw
 
-  const meta = await getWasmMetadata(metaFileBuffer)
-  const programId = "0x223a3aea44276c10764b22eae98769e68144f455ff0b10b8150a6c49e3010ea3"
+  const state = await gearApi.programState.read(programId, metaFileBuffer, {
+    BalanceOf: `${addressRaw}`,
+  })
 
-  try {
-    const message = {
-      destination: programId,
-      payload: payload,
-      gasLimit: 2000_000_000,
-      value: 0
-    }
+  const daoBalance = state.toHuman()
+  console.log(daoBalance)
 
-    const res = await gearApi.message.submit(message, meta)
-
-  } catch (error) {
-    console.error(`${error}`)
-    exit(233)
-  }
-
-  try {
-   await gearApi.message.signAndSend(keyring, async (data) => {
-      console.log("********")
-      console.log({data})
-      console.log("********")
-    })
-
-  } catch (error) {
-    console.error(`${error}`)
-  }
-
-  console.log()
-  exit(0)
 }
 
-sendMsg()
+balance()
